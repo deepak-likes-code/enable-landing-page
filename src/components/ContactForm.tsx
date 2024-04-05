81b6d84994b4d62d6d815ab58fec5ea02229e3e2
Deepak <deepakkomma@gmail.com>
2024-04-05 23:37:06 +0530

    changes

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,4 +1,8 @@
 import React, { useState, ChangeEvent, FormEvent } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+// import "react-toastify/dist/ReactToastify.css";
+
 // Define a type for the form state
 type FormState = {
   name: string;
@@ -39,7 +43,9 @@ const ContactForm = () => {
 
       if (response.ok) {
         // Handle success response
-        alert("Details submitted successfully.");
+        toast.success("Submitted succesfully", {
+          autoClose: 2000,
+        });
         setForm({ name: "", email: "", feedback: "" }); // Optionally reset the form
         setSubmitted(true);
       } else {
@@ -48,75 +54,78 @@ const ContactForm = () => {
       }
     } catch (error) {
       console.error("Submission error:", error);
-      alert("An error occurred while submitting the form.");
+      toast.error("An error occurred while submitting the form.");
     }
   };
 
   return (
-    <form
-      className="w-full max-w-4xl mt-20 mx-auto p-5  rounded-sm "
-      style={{ textAlign: "center", fontSize: "0.9em" }}
-      onSubmit={submitForm}
-    >
-      <p className="font-semibold text-2xl mb-6">Get in Touch</p>
-      <div className="flex flex-wrap -mx-3 mb-6 justify-center">
-        <div className="flex flex-col md:flex-row w-full md:space-x-4">
-          <div className="md:w-1/2 px-3 mb-6 md:mb-0">
-            <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="name"
-            >
-              Name
-            </label>
-            <input
-              name="name"
-              type="text"
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white"
-              placeholder="Full Name"
-              onChange={handleChange}
-            />
-            <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2 mt-4"
-              htmlFor="email"
-            >
-              Email
-            </label>
-            <input
-              name="email"
-              type="email"
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white"
-              placeholder="Email"
-              onChange={handleChange}
-            />
-          </div>
-          <div className="md:w-1/2 px-3">
-            <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="feedback"
-            >
-              Feedback
-            </label>
-            <textarea
-              name="feedback"
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white h-32 resize-none"
-              placeholder="Feedback"
-              onChange={handleChange}
-            ></textarea>
+    <>
+      <ToastContainer position="top-left" />
+      <form
+        className="w-full max-w-4xl mt-20 mx-auto p-5  rounded-sm "
+        style={{ textAlign: "center", fontSize: "0.9em" }}
+        onSubmit={submitForm}
+      >
+        <p className="font-semibold text-2xl mb-6">Get in Touch</p>
+        <div className="flex flex-wrap -mx-3 mb-6 justify-center">
+          <div className="flex flex-col md:flex-row w-full md:space-x-4">
+            <div className="md:w-1/2 px-3 mb-6 md:mb-0">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="name"
+              >
+                Name
+              </label>
+              <input
+                name="name"
+                type="text"
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white"
+                placeholder="Full Name"
+                onChange={handleChange}
+              />
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2 mt-4"
+                htmlFor="email"
+              >
+                Email
+              </label>
+              <input
+                name="email"
+                type="email"
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white"
+                placeholder="Email"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="md:w-1/2 px-3">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="feedback"
+              >
+                Feedback
+              </label>
+              <textarea
+                name="feedback"
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white h-32 resize-none"
+                placeholder="Feedback"
+                onChange={handleChange}
+              ></textarea>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="flex justify-center mt-6">
-        <button
-          className={`shadow bg-pink-500 hover:bg-pink-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded ${
-            submitted ? "opacity-50 cursor-not-allowed" : ""
-          }`}
-          type="submit"
-          disabled={submitted}
-        >
-          Send Message
-        </button>
-      </div>
-    </form>
+        <div className="flex justify-center mt-6">
+          <button
+            className={`shadow bg-pink-500 hover:bg-pink-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded ${
+              submitted ? "opacity-50 cursor-not-allowed" : ""
+            }`}
+            type="submit"
+            disabled={submitted}
+          >
+            Send Message
+          </button>
+        </div>
+      </form>
+    </>
   );
 };
 
